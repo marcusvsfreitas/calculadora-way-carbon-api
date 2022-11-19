32df7a039b8a4dad03741f640cfb13e163975431
marcusvsfreitas <marcus.sfreitas@outlook.com>
2022-11-19 08:58:06 -0300

    refactor: add singleton pattern in Funcionarios

--- src/modules/funcionarios/repositories/PrismaFuncionariosRepository.ts
+++ src/modules/funcionarios/repositories/PrismaFuncionariosRepository.ts
@@ -6,6 +6,18 @@ const prisma = new PrismaClient();
 
 
 class PrismaFuncionariosRepository implements IFuncionariosRepository {
+  private static INSTANCE: PrismaFuncionariosRepository;
+
+  public static getInstance(): PrismaFuncionariosRepository {
+    if(!PrismaFuncionariosRepository.INSTANCE){
+      PrismaFuncionariosRepository.INSTANCE = new PrismaFuncionariosRepository();
+    }
+
+    return PrismaFuncionariosRepository.INSTANCE;
+  }
+
+
+
   async create({ matriz, nome, vinculos_id, cargos_id, salario, premio, localidade, data_nascimento, dependentes, tipo_contrato_id, data_admissao, grupo_ocupacional_id, equipe_id, cargo_enquadramento_id }: ICreateFuncionarioDTO): Promise<void> {
 
     const result = await prisma.funcionarios.create({ data: {matriz, nome, vinculos_id, cargos_id, salario, premio, localidade, data_nascimento, dependentes, tipo_contrato_id, data_admissao, grupo_ocupacional_id, equipe_id, cargo_enquadramento_id }});
--- src/modules/funcionarios/useCases/createFuncionario/index.ts
+++ src/modules/funcionarios/useCases/createFuncionario/index.ts
@@ -2,7 +2,7 @@ import { PrismaFuncionariosRepository } from "../../repositories/PrismaFuncionar
 import { CreateFuncionarioController } from "./CreateFuncionarioController";
 import { CreateFuncionarioUseCase } from "./CreateFuncionarioUseCase";
 
-const funcionariosRepository = new PrismaFuncionariosRepository();
+const funcionariosRepository = PrismaFuncionariosRepository.getInstance();
 const createFuncionarioUseCase = new CreateFuncionarioUseCase(funcionariosRepository);
 const createFuncionarioController = new CreateFuncionarioController(createFuncionarioUseCase);
 
